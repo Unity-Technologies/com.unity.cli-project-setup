7a0bc9b97bb93e203b22cc3bb4565e784e77a407
Sean Stolberg <seans@unity3d.com>
2020-05-26 09:58:45 -0700

    updates to work with logging perf

---
+++ Editor/CliProjectSetup.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using NDesk.Options;
+using Unity.Burst;
+using UnityEngine;
+using UnityEngine.Rendering;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace com.unity.cliprojectsetup
+{
+    public class CliProjectSetup
+    {
+        private readonly Regex customArgRegex = new Regex("-([^=]*)=", RegexOptions.Compiled);
+        private readonly PlatformSettings platformSettings = new PlatformSettings();
+
+        public void ConfigureFromCmdlineArgs()
+        {
+#if UNITY_EDITOR
+            ParseCommandLineArgs();
+            ConfigureSettings();
+#endif
+        }
+#if UNITY_EDITOR
+        private void ParseCommandLineArgs()
+        {
+            var args = Environment.GetCommandLineArgs();
+            EnsureOptionsLowerCased(args);
+            var optionSet = DefineOptionSet();
+            var unParsedArgs = optionSet.Parse(args);
+            platformSettings.SerializeToAsset();
+        }
+
+        private void EnsureOptionsLowerCased(string[] args)
+        {
+            for (var i = 0; i < args.Length; i++)
+            {
+                if (customArgRegex.IsMatch(args[i]))
+                {
+                    args[i] = customArgRegex.Replace(args[i], customArgRegex.Matches(args[i])[0].ToString().ToLower());
+                }
+            }
+        }
+
+        private void ConfigureSettings()
+        {
+            // Setup all-inclusive player settings
+            ConfigureCrossplatformSettings();
+        }
+
+        private void ConfigureCrossplatformSettings()
+        {
+            if (platformSettings.PlayerGraphicsApi != GraphicsDeviceType.Null)
+            {
+                PlayerSettings.SetUseDefaultGraphicsAPIs(platformSettings.BuildTarget, false);
+                PlayerSettings.SetGraphicsAPIs(platformSettings.BuildTarget,
+                    new[] {platformSettings.PlayerGraphicsApi});
+            }
+
+            PlayerSettings.colorSpace = platformSettings.ColorSpace;
+
+            PlayerSettings.SetScriptingBackend(EditorUserBuildSettings.selectedBuildTargetGroup,
+                platformSettings.ScriptingImplementation);
+
+            BurstCompiler.Options.EnableBurstCompilation = platformSettings.EnableBurst;
+        }
+
+        private OptionSet DefineOptionSet()
+        {
+            var optionsSet = new OptionSet();
+            optionsSet.Add("scriptingbackend=",
+                "Scripting backend to use. IL2CPP is default. Values: IL2CPP, Mono", ParseScriptingBackend);
+            optionsSet.Add("playergraphicsapi=", "Graphics API based on GraphicsDeviceType.",
+                graphicsDeviceType =>
+                    platformSettings.PlayerGraphicsApi = TryParse<GraphicsDeviceType>(graphicsDeviceType));
+            optionsSet.Add("colorspace=", "Linear or Gamma color space.",
+                colorSpace => platformSettings.ColorSpace = TryParse<ColorSpace>(colorSpace));
+            optionsSet.Add("mtRendering",
+                "Enable or disable multithreaded rendering. Enabled is default. Use option to enable, or use option and append '-' to disable.",
+                option => platformSettings.MtRendering = option != null);
+            optionsSet.Add("graphicsJobs",
+                "Enable graphics jobs rendering. Disabled is default. Use option to enable, or use option and append '-' to disable.",
+                option => platformSettings.GraphicsJobs = option != null);
+            optionsSet.Add("testsrev=",
+                "revision id of the tests being used.",
+                testsrev => platformSettings.TestsRevision = testsrev);
+            optionsSet.Add("testsrevdate=",
+                "revision date of the tests being used.",
+                revDate => platformSettings.TestsRevisionDate = revDate);
+            optionsSet.Add("testsbranch=",
+                "branch of the tests repo being used.",
+                testsbranch => platformSettings.TestsBranch = testsbranch);
+            optionsSet.Add("enableburst",
+                "Enable burst. Enabled is default. Use option to disable: -enableburst",
+                option => platformSettings.EnableBurst = option != null);
+            optionsSet.Add("packageundertestname=",
+                "logging commit revision id",
+                packageundertestname => platformSettings.PackageUnderTestName = packageundertestname);
+            optionsSet.Add("packageundertestrev=",
+                "logging commit revision id",
+                packageundertestrev => platformSettings.PackageUnderTestRevision = packageundertestrev);
+            optionsSet.Add("packageundertestrevdate=",
+                "logging commit revision date",
+                packageundertestrevdate => platformSettings.PackageUnderTestRevisionDate = packageundertestrevdate);
+            optionsSet.Add("packageundertestbranch=",
+                "branch of the logging repo being used.",
+                packageundertestbranch => platformSettings.PackageUnderTestBranch = packageundertestbranch);
+            return optionsSet;
+        }
+
+        public static T TryParse<T>(string stringToParse)
+        {
+            T thisType;
+            try
+            {
+                thisType = (T) Enum.Parse(typeof(T), stringToParse);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException($"Couldn't cast {stringToParse} to {typeof(T)}", e);
+            }
+
+            return thisType;
+        }
+
+        private void ParseScriptingBackend(string scriptingBackend)
+        {
+            var sb = scriptingBackend.ToLower();
+            if (sb.Equals("mono"))
+            {
+                platformSettings.ScriptingImplementation = ScriptingImplementation.Mono2x;
+            }
+        }
+#endif
+    }
+}
--- Editor/PlatformSettings.cs
+++ Editor/PlatformSettings.cs
@@ -1,102 +1,67 @@
-﻿#if OCULUS_SDK
-using Unity.XR.Oculus;
-#endif
-using System;
+﻿using System;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
-using com.unity.xr.test.runtimesettings;
+using com.unity.test.performance.runtimesettings;
+using UnityEngine;
+using UnityEngine.Rendering;
 #if UNITY_EDITOR
+using System.Text;
 using UnityEditor;
 using UnityEditor.PackageManager;
 #if URP
 using UnityEngine.Rendering.Universal;
 #endif
 #endif
-using UnityEngine;
-using UnityEngine.Rendering;
-using PackageInfo = UnityEditor.PackageManager.PackageInfo;
-#if ENABLE_VR
-using UnityEngine.XR;
-#endif
 
-
-namespace com.unity.cliconfigmanager
+namespace com.unity.cliprojectsetup
 {
     public class PlatformSettings
     {
+// TODO, make sure this wrap is correct
 #if UNITY_EDITOR
-        public BuildTargetGroup BuildTargetGroup => EditorUserBuildSettings.selectedBuildTargetGroup;
         public BuildTarget BuildTarget => EditorUserBuildSettings.activeBuildTarget;
-
-        public string XrTarget;
         public GraphicsDeviceType PlayerGraphicsApi;
-
-#if OCULUS_SDK
-        public OculusSettings.StereoRenderingModeDesktop StereoRenderingModeDesktop;
-        public OculusSettings.StereoRenderingModeAndroid StereoRenderingModeAndroid;
-#else
-        public StereoRenderingPath StereoRenderingPath;
+        public string PackageUnderTestName;
+        public string PackageUnderTestRevision;
+        public string PackageUnderTestRevisionDate;
+        public string PackageUnderTestBranch;
 #endif
-        public bool MtRendering = true;
-        public bool GraphicsJobs;
-        public AndroidSdkVersions MinimumAndroidSdkVersion = AndroidSdkVersions.AndroidApiLevel24;
-        public AndroidSdkVersions TargetAndroidSdkVersion = AndroidSdkVersions.AndroidApiLevelAuto;
-        public ScriptingImplementation ScriptingImplementation = ScriptingImplementation.IL2CPP;
-        public string AppleDeveloperTeamId;
-        public string IOsProvisioningProfileId;
-        public ColorSpace ColorSpace = ColorSpace.Gamma;
-        public string XrsdkRevision;
-        public string XrsdkRevisionDate;
-        public string XrsdkBranch;
-        public string TestsRevision;
-        public string TestsRevisionDate;
-        public string TestsBranch;
-        public string DeviceRuntimeVersion;
-        public string SimulationMode;
-        public string Username;
-        public string RenderPipeline;
-        public string FfrLevel;
-        public AndroidArchitecture AndroidTargetArchitecture = AndroidArchitecture.ARM64;
-
         private readonly string resourceDir = "Assets/Resources";
-        private readonly string xrManagementPackageName = "com.unity.xr.management";
-        private readonly string perfTestsPackageName = "xr.sdk.oculus.performancetests";
-        private readonly string urpPackageName = "com.unity.render-pipelines.universal";
-        private readonly string oculusXrSdkPackageName = "com.unity.xr.oculus";
-        private readonly string hdrpPackageName = "com.unity.testing.hdrp";
 
         private readonly Regex revisionValueRegex = new Regex("\"revision\": \"([a-f0-9]*)\"",
             RegexOptions.Compiled | RegexOptions.IgnoreCase);
         private readonly Regex majorMinorVersionValueRegex = new Regex("([0-9]*\\.[0-9]*\\.)",
             RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
+        public ColorSpace ColorSpace = ColorSpace.Gamma;
+        public bool EnableBurst = true;
+        public bool GraphicsJobs;
+        public bool MtRendering = true;
+        public string RenderPipeline;
+        public ScriptingImplementation ScriptingImplementation = ScriptingImplementation.IL2CPP;
+        public string TestsBranch;
+        public string TestsRevision;
+        public string TestsRevisionDate;
+        public string Username;
 
         public void SerializeToAsset()
         {
             var settingsAsset = ScriptableObject.CreateInstance<CurrentSettings>();
 
-            settingsAsset.SimulationMode = SimulationMode;
             settingsAsset.PlayerGraphicsApi = PlayerGraphicsApi.ToString();
             settingsAsset.MtRendering = MtRendering;
             settingsAsset.GraphicsJobs = GraphicsJobs;
+            settingsAsset.EnableBurst = EnableBurst;
             settingsAsset.ColorSpace = ColorSpace.ToString();
-            settingsAsset.EnabledXrTarget = XrTarget;
-            settingsAsset.XrsdkRevision = GetOculusXrSdkPackageVersionInfo();
-            settingsAsset.XrManagementRevision = GetXrManagementPackageVersionInfo();
-            settingsAsset.UrpPackageVersionInfo = GetUrpPackageVersionInfo();
-            settingsAsset.HdrpPackageVersionInfo = GetHdrpPackageVersionInfo();
-            settingsAsset.PerfTestsPackageRevision = GetPerfTestsPackageVersionInfo();
-            settingsAsset.DeviceRuntimeVersion = DeviceRuntimeVersion;
             settingsAsset.Username = Username = Environment.UserName;
-            settingsAsset.FfrLevel = FfrLevel;
+            settingsAsset.PackageUnderTestName = PackageUnderTestName;
             settingsAsset.TestsRevision = TestsRevision;
             settingsAsset.TestsRevisionDate = TestsRevisionDate;
             settingsAsset.TestsBranch = TestsBranch;
-            settingsAsset.AndroidTargetArchitecture = string.Format("AndroidTargetArchitecture|{0}", AndroidTargetArchitecture.ToString());
-            settingsAsset.RenderPipeline = RenderPipeline =
-                $"renderpipeline|{(GraphicsSettings.renderPipelineAsset != null ? GraphicsSettings.renderPipelineAsset.name : "BuiltInRenderer")}";
+            GetPackageUnderTestVersionInfo(settingsAsset);
+            settingsAsset.RenderPipeline = RenderPipeline =  $"{(GraphicsSettings.renderPipelineAsset != null ? GraphicsSettings.renderPipelineAsset.name : "BuiltInRenderer")}";
 
 #if URP
             settingsAsset.AntiAliasing = GraphicsSettings.renderPipelineAsset != null
@@ -105,181 +70,65 @@ namespace com.unity.cliconfigmanager
 #else
             settingsAsset.AntiAliasing = QualitySettings.antiAliasing;
 #endif
-            
-
-#if OCULUS_SDK
-        settingsAsset.StereoRenderingModeDesktop = StereoRenderingModeDesktop.ToString();
-            settingsAsset.StereoRenderingModeAndroid = StereoRenderingModeAndroid.ToString();
-#if OCULUS_SDK_PERF
-            settingsAsset.PluginVersion = string.Format("OculusPluginVersion|{0}", OculusStats.PluginVersion);
-#endif
-#else
-            settingsAsset.StereoRenderingMode = GetXrStereoRenderingPathMapping(StereoRenderingPath).ToString();
-#endif
             CreateAndSaveCurrentSettingsAsset(settingsAsset);
         }
 
-        private string GetXrManagementPackageVersionInfo()
+        private void GetPackageUnderTestVersionInfo(CurrentSettings settingsAsset)
         {
-            string packageRevision = string.Empty;
-
             var listRequest = Client.List(true);
             while (!listRequest.IsCompleted)
             {
             }
 
-            if (listRequest.Result.Any(r => r.name.Equals(xrManagementPackageName)))
+            if (listRequest.Result.Any(r => r.name.Equals(PackageUnderTestName)))
             {
-                var xrManagementPckg =
-                    listRequest.Result.First(r => r.name.Equals(xrManagementPackageName));
+                var packageUnderTestInfo =
+                    listRequest.Result.First(r => r.name.Equals(PackageUnderTestName));
 
-                var revision = TryGetRevisionFromPackageJson(xrManagementPackageName) ?? "unavailable";
-                var version = xrManagementPckg.version;
-                packageRevision = string.Format("XrManagementPackageName|{0}|XrManagementVersion|{1}|XrManagementRevision|{2}", xrManagementPackageName, version, revision);
-            }
+                settingsAsset.PackageUnderTestVersion = packageUnderTestInfo.version;
 
-            return packageRevision;
-        }
-
-        private string GetUrpPackageVersionInfo()
-        {
-            string packageRevision = string.Empty;
-
-            var listRequest = Client.List(true);
-            while (!listRequest.IsCompleted)
-            {
-            }
-
-            if (listRequest.Result.Any(r => r.name.Equals(urpPackageName)))
-            {
-                var urpPckg =
-                    listRequest.Result.First(r => r.name.Equals(urpPackageName));
-
-                var revision = TryGetRevisionFromPackageJson(urpPackageName) ?? "unavailable";
-                var version = urpPckg.version;
-                packageRevision = string.Format("UrpPackageName|{0}|UrpVersion|{1}|UrpRevision|{2}", urpPackageName, version, revision);
-            }
-
-            return packageRevision;
-        }
-
-        private string GetHdrpPackageVersionInfo()
-        {
-            string packageRevision = string.Empty;
-
-            var listRequest = Client.List(true);
-            while (!listRequest.IsCompleted)
-            {
-            }
-
-            if (listRequest.Result.Any(r => r.name.Equals(hdrpPackageName)))
-            {
-                var urpPckg =
-                    listRequest.Result.First(r => r.name.Equals(hdrpPackageName));
-
-                var revision = TryGetRevisionFromPackageJson(hdrpPackageName) ?? "unavailable";
-                var version = urpPckg.version;
-                packageRevision = string.Format("HdrpPackageName|{0}|HdrpVersion|{1}|HdrpRevision|{2}", hdrpPackageName, version, revision);
-            }
-
-            return packageRevision;
-        }
-
-        private string GetPerfTestsPackageVersionInfo()
-        {
-            string packageRevision = string.Empty;
-
-            var listRequest = Client.List(true);
-            while (!listRequest.IsCompleted)
-            {
-            }
-
-            if (listRequest.Result.Any(r => r.name.Equals(perfTestsPackageName)))
-            {
-                var perfTestsPckg =
-                    listRequest.Result.First(r => r.name.Equals(perfTestsPackageName));
-
-                var revision = TryGetRevisionFromPackageJson(perfTestsPackageName) ?? "unavailable";
-                var version = perfTestsPckg.version;
-                packageRevision = string.Format("PerfTestsPackageName|{0}|PerfTestsVersion|{1}|PerfTestsRevision|{2}", perfTestsPackageName, version, revision);
-            }
-
-            return packageRevision;
-        }
-
-        private string GetOculusXrSdkPackageVersionInfo()
-        {
-            string packageRevision = string.Empty;
-
-            var listRequest = Client.List(true);
-            while (!listRequest.IsCompleted)
-            {
-            }
-
-            if (listRequest.Result.Any(r => r.name.Equals(oculusXrSdkPackageName)))
-            {
-                var oculusXrsdkPckg =
-                    listRequest.Result.First(r => r.name.Equals(oculusXrSdkPackageName));
-
-                var version = oculusXrsdkPckg.version;
-
-                // if XrsdkRevision is empty, then it wasn't passed in on the command line (which is
-                // usually going to be the case if we're running in tests at the PR level for Xrsdk package).
+                // if PackageRevision is empty, then it wasn't passed in on the command line (which is
+                // usually going to be the case if we're running in tests at the PR level for the package).
                 // In this case, we most likely are using a released package reference, so let's try to get
                 // the revision from the package.json.
-                if (string.IsNullOrEmpty(XrsdkRevision))
-                {
-                    XrsdkRevision = TryGetRevisionFromPackageJson(oculusXrSdkPackageName) ?? "unavailable";
-                }
+                settingsAsset.PackageUnderTestRevision = string.IsNullOrEmpty(PackageUnderTestRevision) ? 
+                    TryGetRevisionFromPackageJson(PackageUnderTestRevision) 
+                    : PackageUnderTestRevision;
 
-                // if XrsdkRevisionDate is empty, then it wasn't passed in on the command line (which is
-                // usually going to be the case if we're running in tests at the PR level for Xrsdk package).
+                // if PackageUnderTestRevisionDate is empty, then it wasn't passed in on the command line (which is
+                // usually going to be the case if we're running in tests at the PR level for the package).
                 // In this case, we most likely are using a released package reference, so let's try to get
                 // the revision date from the package manager api instead.
-                if (string.IsNullOrEmpty(XrsdkRevisionDate))
-                {
-                    TryGetXrsdkRevisionDate(oculusXrsdkPckg);
-                }
+                settingsAsset.PackageUnderTestRevisionDate = string.IsNullOrEmpty(PackageUnderTestRevisionDate)
+                    ? TryGetPackageUnderTestRevisionDate(packageUnderTestInfo.datePublished)
+                    : PackageUnderTestRevisionDate;
 
-                // if XrsdkBranch is empty, then it wasn't passed in on the command line (which is
-                // usually going to be the case if we're running in tests at the PR level for Xrsdk package).
+                // if PackageUnderTestBranch is empty, then it wasn't passed in on the command line (which is
+                // usually going to be the case if we're running in tests at the PR level for the package).
                 // In this case, we most likely are using a released package reference, so let's try to infer
                 // the branch from the major.minor version of the package via the package manager API
-                if (string.IsNullOrEmpty(XrsdkBranch))
-                {
-                    TryGetXrsdkBranch(oculusXrsdkPckg);
-                }
-                packageRevision = string.Format(
-                    "XrSdkName|{0}|XrSdkVersion|{1}|XrSdkRevision|{2}|XrSdkRevisionDate|{3}|XrSdkBranch|{4}", 
-                    oculusXrSdkPackageName, 
-                    version, 
-                    XrsdkRevision, 
-                    XrsdkRevisionDate, 
-                    XrsdkBranch);
+                settingsAsset.PackageUnderTestPackageBranch = string.IsNullOrEmpty(PackageUnderTestBranch)
+                    ? TryGetPackageUnderTestBranch(packageUnderTestInfo.version)
+                    : PackageUnderTestBranch;
             }
-
-            return packageRevision;
         }
 
-        private void TryGetXrsdkBranch(PackageInfo oculusXrsdkPckg)
+        private string TryGetPackageUnderTestBranch(string version)
         {
-            var matches = majorMinorVersionValueRegex.Matches(oculusXrsdkPckg.version);
-            XrsdkBranch = matches.Count > 0 ? string.Concat(matches[0].Groups[0].Value, "x") : "release";
+            var matches = majorMinorVersionValueRegex.Matches(version);
+            return matches.Count > 0 ? string.Concat(matches[0].Groups[0].Value, "x") : "release";
         }
 
-        private void TryGetXrsdkRevisionDate(PackageInfo oculusXrsdkPckg)
+        private string TryGetPackageUnderTestRevisionDate(DateTime? datePublished)
         {
-#if OCULUS_SDK
-            XrsdkRevisionDate = 
-                oculusXrsdkPckg.datePublished != null ? 
-                    ((DateTime) oculusXrsdkPckg.datePublished).ToString("s", DateTimeFormatInfo.InvariantInfo) : "unavailable";
-#endif
+            return datePublished != null ?
+                    ((DateTime)datePublished).ToString("s", DateTimeFormatInfo.InvariantInfo) : "unavailable";
         }
 
         private string TryGetRevisionFromPackageJson(string packageName)
         {
             string revision = null;
-            var packageAsString = File.ReadAllText(string.Format("Packages/{0}/package.json",packageName));
+            var packageAsString = File.ReadAllText(string.Format("Packages/{0}/package.json", packageName));
             var matches = revisionValueRegex.Matches(packageAsString);
             if (matches.Count > 0)
             {
@@ -289,31 +138,15 @@ namespace com.unity.cliconfigmanager
             return revision;
         }
 
-        private XRSettings.StereoRenderingMode GetXrStereoRenderingPathMapping(StereoRenderingPath stereoRenderingPath)
-        {
-            switch (stereoRenderingPath)
-            {
-                case StereoRenderingPath.SinglePass:
-                    return XRSettings.StereoRenderingMode.SinglePass;
-                case StereoRenderingPath.MultiPass:
-                    return XRSettings.StereoRenderingMode.MultiPass;
-                case StereoRenderingPath.Instancing:
-                    return XRSettings.StereoRenderingMode.SinglePassInstanced;
-                default:
-                    return XRSettings.StereoRenderingMode.SinglePassMultiview;
-            }
-        }
-
         private void CreateAndSaveCurrentSettingsAsset(CurrentSettings settingsAsset)
         {
-            if (!System.IO.Directory.Exists(resourceDir))
+            if (!Directory.Exists(resourceDir))
             {
-                System.IO.Directory.CreateDirectory(resourceDir);
+                Directory.CreateDirectory(resourceDir);
             }
 
             AssetDatabase.CreateAsset(settingsAsset, resourceDir + "/settings.asset");
             AssetDatabase.SaveAssets();
         }
-#endif
     }
 }

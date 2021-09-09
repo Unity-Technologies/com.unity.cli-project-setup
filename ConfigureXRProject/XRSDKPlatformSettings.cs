7853b26e50a2d82acbf2c7fdd928e2ef88124ddb
Ejaz Ahmed Mudassir <ejaz.mudassir@unity3d.com>
2021-09-08 17:38:32 -0700

    Update changes with comments

--- ConfigureXRProject/XRSDKPlatformSettings.cs
+++ ConfigureXRProject/XRSDKPlatformSettings.cs
@@ -108,13 +108,19 @@ namespace ConfigureXRProject
             var settingsPath = Path.GetDirectoryName(testXrGeneralSettingsPath);
             if (!AssetDatabase.IsValidFolder(settingsPath))
             {
+                // The parent folder must already exist before creating the folder
+                // Thus itereate through each folder in the hierarchy and create each individually
+                // Ref: https://docs.unity3d.com/2021.2/Documentation/ScriptReference/AssetDatabase.CreateFolder.html 
                 var folders = settingsPath.Split(Path.DirectorySeparatorChar);
                 for (int i=0; i < folders.Length; i++)
                 {
                     if (!AssetDatabase.IsValidFolder(folders[i]))
                     {
                         var parentFolder = string.Join(Path.DirectorySeparatorChar, folders, 0, i);
-                        AssetDatabase.CreateFolder(parentFolder, folders[i]);
+                        if ( string.IsNullOrEmpty(AssetDatabase.CreateFolder(parentFolder, folders[i])) )
+                        {
+                            throw new Exception(string.Format("Failed to create folder {0}/{1}", parentFolder, folders[i]));
+                        }
                     }
                 }
             }
